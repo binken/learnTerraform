9f3f3d99ea9d601b6e5d42040742c9c224938593
binkenzou <binken.zou@gmail.com>
2023-02-24 21:28:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,116 +1,24 @@
-# 1. Specify the version of the AzureRM Provider to use
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "=3.0.1"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
     }
   }
+
+  required_version = ">= 1.1.0"
 }
 
-# 2. Configure the AzureRM Provider
 provider "azurerm" {
+  features {}
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  features {}
 }
 
-variable "subscription_id" {
-  type        = string
-  description = "The Azure AD client secret used to authenticate with the Azure API"
-  default = ""
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
 
-variable "client_id" {
-  type        = string
-  description = "The Azure AD client secret used to authenticate with the Azure API"
-  default = ""
-}
-
-variable "client_secret" {
-  type        = string
-  description = "The Azure AD client secret used to authenticate with the Azure API"
-  default = ""
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "tenant_id"
-  default = ""
-}
-
-variable "admin_password" {
-  type        = string
-  description = "admin_password"
-  default = ""
-}
-
-
-
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "eastus"
-}
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-virtual-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-network-interface"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "example-ip-configuration"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-
-  vm_size              = "Standard_DS1_v2"
-  delete_os_disk_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "example-os-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "example-vm"
-    admin_username = "adminuser"
-    admin_password = var.admin_password
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-}
