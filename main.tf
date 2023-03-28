a8123197241b145c5727523af5cfa8807b0eebb5
binkenzou <binken.zou@gmail.com>
2023-03-28 15:54:35 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,36 +31,3 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Define the virtual machine
-resource "azurerm_virtual_machine" "vm" {
-  name                  = var.vm_name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = var.vm_size
-
-  storage_image_reference {
-    publisher = var.image_publisher
-    offer     = var.image_offer
-    sku       = var.image_sku
-    version   = var.image_version
-  }
-
-  storage_os_disk {
-    name              = var.os_disk_name
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = var.vm_name
-    admin_username = var.admin_username
-    admin_password = var.admin_password
-  }
-
-  os_profile_windows_config {
-    enable_automatic_upgrades = true
-    provision_vm_agent        = true
-  }
-}
