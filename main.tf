00ad6983789292f888a31703516743e21a74a75f
binkenzou <binken.zou@gmail.com>
2023-03-28 15:44:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,32 +18,36 @@ resource "azurerm_resource_group" "rg" {
   location = "westus2"
 }
 
-variable "subscription_id" {
-  type        = string
-  description = "The Azure AD client secret used to authenticate with the Azure API"
-  default = ""
-}
+# Define the virtual machine
+resource "azurerm_virtual_machine" "vm" {
+  name                  = var.vm_name
+  location              = var.location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
 
-variable "client_id" {
-  type        = string
-  description = "The Azure AD client secret used to authenticate with the Azure API"
-  default = ""
-}
+  storage_image_reference {
+    publisher = var.image_publisher
+    offer     = var.image_offer
+    sku       = var.image_sku
+    version   = var.image_version
+  }
 
-variable "client_secret" {
-  type        = string
-  description = "The Azure AD client secret used to authenticate with the Azure API"
-  default = ""
-}
+  storage_os_disk {
+    name              = var.os_disk_name
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
 
-variable "tenant_id" {
-  type        = string
-  description = "tenant_id"
-  default = ""
-}
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
 
-variable "admin_password" {
-  type        = string
-  description = "admin_password"
-  default = ""
+  os_profile_windows_config {
+    enable_automatic_upgrades = true
+    provision_vm_agent        = true
+  }
 }
