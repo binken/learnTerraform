327d62e6e5cdb4a10f46ae2fc626ba230e5d4a5f
binkenzou <binken.zou@gmail.com>
2023-03-28 16:00:07 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,8 +14,8 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+  name     = "var.resource_group_name"
+  location = "var.location"
 }
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-vm-vnet"
@@ -43,3 +43,36 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+resource "azurerm_public_ip" "publicip" {
+  name                = "my-vm-publicip"
+  location            = "${azurerm_resource_group.rg.location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm1"
+  location              = "${azurerm_resource_group.rg.location}"
+  resource_group_name   = "${azurerm_resource_group.rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.nic.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "my-vm-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "vm1"
+    admin_username = "adminuser"
+    admin_password = "P@ssw0rd1234!"
+  }
