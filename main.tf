3ef18c8a99cf976e071bbd2fc380742c3b818a00
binkenzou <binken.zou@gmail.com>
2023-03-29 13:35:57 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = "${azurerm_resource_group.rg.name}"
 }
 
+resource "azurerm_subnet" "AzureFireWallSubnet" {
+  name                 = "AzureFireWallSubnet"
+  address_prefixes     = ["10.0.3.0/24"]
+  virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+  resource_group_name  = "${azurerm_resource_group.rg.name}"
+}
+
 resource "azurerm_public_ip" "publicip" {
   name                = "my-vm-publicip"
   location            = "${azurerm_resource_group.rg.location}"
