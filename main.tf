a50cb6e840fd63c092ebd4cc100d5802b6cd80dc
binkenzou <binken.zou@gmail.com>
2023-03-29 12:59:02 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,7 +51,16 @@ resource "azurerm_network_interface" "nic" {
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
+resource "azurerm_network_security_group" "nsgsubnet" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
 
+resource "azurerm_subnet_network_security_group_association" "nsgsubnetas" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsgsubnet.id
+}
 
 
 resource "azurerm_virtual_machine" "vm" {
