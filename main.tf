88393cec6911ac6c4678d256238638c073560729
binkenzou <binken.zou@gmail.com>
2023-03-29 10:13:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ resource "azurerm_public_ip" "publicip" {
   name                = "my-vm-publicip"
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 
 }
