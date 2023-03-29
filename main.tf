941c4e1334dc1a6cc4e959a30cdda811975d7f17
binkenzou <binken.zou@gmail.com>
2023-03-29 10:27:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,14 +48,8 @@ resource "azurerm_network_interface" "nic" {
     name                          = "my-nic-ipconfig"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-  }
-  ip_configuration {
-    name                          = "publicipconfiguration1"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.publicip.id
   }
-
 }
 
 
